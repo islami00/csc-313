b40fb974938cbcb7d2a380865e24d053238d8797
Islam <islamkwnd@gmail.com>
2022-12-02 21:10:31 +0100

    feat: Add db example

--- project/admin/db_conn.php
+++ project/admin/db_conn.php
@@ -9,3 +9,19 @@ $dsn = "mysql:host=${host};dbname=${dbname}";
 $connection = new PDO($dsn, $user);
 $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 $connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+// PDO QUERY
+// :x means named parameters
+// $sql = 'INSERT INTO files VALUES (DEFAULT, :title, :path, :level);';
+// $stmt = $connection->prepare($sql);
+// $result = false;
+// if ($stmt) {
+// You define the parameters here in an associative array
+//   $result = $stmt->execute([':title' => $file_name, ':path' => $target_path, ':level' => $level]);
+// }
+// $result is true if it succeeds, react as needed
+// if ($result) {
+//   echo "Success";
+// } else {
+//   echo "Failure";
+// }
