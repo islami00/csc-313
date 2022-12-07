f1ff0ab1e137cc6881cdec56841e25720cd76c73
Islam <islamkwnd@gmail.com>
2022-12-07 17:20:04 +0100

    feat: Add aliya work, wire things up

--- project/students/index.php
+++ project/students/index.php
@@ -27,11 +27,8 @@ $result = [];
 $message = null;
 try {
   $result  = get_topics();
-  if (gettype($result) === "string") {
-    $message =  $result;
-  }
 } catch (\Throwable $th) {
-  echo "Error getting course content";
+  $result  = "Error getting course content";
 }
 
 ?>
@@ -42,13 +39,43 @@ try {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Student view</title>
+  <svg width="0" height="0" class="hidden">
+    <symbol xmlns="http://www.w3.org/2000/svg" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24" id="download">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"></path>
+      <polyline points="7 11 12 16 17 11"></polyline>
+      <line x1="12" y1="4" x2="12" y2="16"></line>
+    </symbol>
+  </svg>
 </head>
 
 <body>
-  <?php if (isset($message)) : ?>
-    <p><?php echo $message ?></p>
-  <?php endif ?>
+
   <a href="<?php echo get_path("/logout.php"); ?>">Logout</a>
+  <div class="icon-wrap">
+    <div class="title-list">
+      <?php if (gettype($result) === "array") : ?>
+        <?php foreach ($result as $result_item) : ?>
+          <?php
+          $name = $result_item->uploaded_file_name;
+          $title = $result_item->title;
+          $path =  get_upload_path("/${name}");
+          ?>
+
+          <div class="title-row">
+            <p><?php echo $title ?></p>
+            <a class="icon-small" href="<?php echo $path ?>">
+              <svg class="icon">
+                <use xlink:href="#download"></use>
+              </svg>
+            </a>
+          </div>
+        <?php endforeach ?>
+      <?php endif ?>
+      <?php if (gettype($result) === "string") : ?>
+        <p><?php echo $result ?></p>
+      <?php endif ?>
+    </div>
 </body>
 
 
